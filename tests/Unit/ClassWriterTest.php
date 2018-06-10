997be3b1cd9d3d3712376ff61ccfea2d651442f5
Bohuslav Šimek <bohuslav@simek.si>
2018-06-10 10:09:47 +0200

    Move "unit" test to own folder

--- tests/Unit/ClassWriterTest.php
+++ tests/Unit/ClassWriterTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Kambo\Tests\Asm;
+namespace Kambo\Tests\Unit\Asm;
 
 use PHPUnit\Framework\TestCase;
 
@@ -30,7 +30,7 @@ class ClassWriterTest extends TestCase
     public function testGenerateHelloWorld()
     {
         $cw = ClassWriter::constructor__I(0);
-        $cw->visit(Opcodes::V1_1, Opcodes::ACC_PUBLIC, "Example", null, "java/lang/Object", null);
+        $cw->visit(Opcodes::V1_8, Opcodes::ACC_PUBLIC, "Example", null, "java/lang/Object", null);
 
         $mw = $cw->visitMethod(Opcodes::ACC_PUBLIC, "<init>", "()V", null, null);
         $mw->visitVarInsn(Opcodes::ALOAD, 0);
@@ -52,7 +52,7 @@ class ClassWriterTest extends TestCase
         $code = $cw->toByteArray();
 
         $expectedClassStructure = [
-            202, 254, 186, 190, 0, 3, 0, 45, 0, 26, 1, 0, 7, 69, 120, 97, 109, 112, 108, 101, 7, 0,
+            202, 254, 186, 190, 0, 0, 0, 52, 0, 26, 1, 0, 7, 69, 120, 97, 109, 112, 108, 101, 7, 0,
             1, 1, 0, 16, 106, 97, 118, 97, 47, 108, 97, 110, 103, 47, 79, 98, 106, 101, 99, 116, 7,
             0, 3, 1, 0, 6, 60, 105, 110, 105, 116, 62, 1, 0, 3, 40, 41, 86, 12, 0, 5, 0, 6, 10, 0, 4,
             0, 7, 1, 0, 4, 109, 97, 105, 110, 1, 0, 22, 40, 91, 76, 106, 97, 118, 97, 47, 108, 97, 110,
@@ -63,10 +63,10 @@ class ClassWriterTest extends TestCase
             100, 33, 8, 0, 17, 1, 0, 19, 106, 97, 118, 97, 47, 105, 111, 47, 80, 114, 105, 110, 116, 83,
             116, 114, 101, 97, 109, 7, 0, 19, 1, 0, 7, 112, 114, 105, 110, 116, 108, 110, 1, 0, 21, 40,
             76, 106, 97, 118, 97, 47, 108, 97, 110, 103, 47, 83, 116, 114, 105, 110, 103, 59, 41, 86,
-            12, 0, 21, 0, 22, 10, 0, 20, 0, 23, 1, 0, 4, 67, 111, 100, 101, 0, 1, 0, 2, 0, 4, 0, NULL,
+            12, 0, 21, 0, 22, 10, 0, 20, 0, 23, 1, 0, 4, 67, 111, 100, 101, 0, 1, 0, 2, 0, 4, 0, null,
             0, 0, 0, 2, 0, 1, 0, 5, 0, 6, 0, 1, 0, 25, 0, 0, 0, 17, 0, 1, 0, 1, 0, 0, 0, 5, 42, 183, 0,
-            8, 177, 0, NULL, 0, 0, 0, 9, 0, 9, 0, 10, 0, 1, 0, 25, 0, 0, 0, 21, 0, 2, 0, 2, 0, 0, 0, 9,
-            178, 0, 16, 18, 18, 182, 0, 24, 177, 0, NULL, 0, 0, 0, 0,
+            8, 177, 0, null, 0, 0, 0, 9, 0, 9, 0, 10, 0, 1, 0, 25, 0, 0, 0, 21, 0, 2, 0, 2, 0, 0, 0, 9,
+            178, 0, 16, 18, 18, 182, 0, 24, 177, 0, null, 0, 0, 0, 0,
         ];
 
         $this->assertEquals($expectedClassStructure, $code);
