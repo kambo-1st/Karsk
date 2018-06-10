c22f5f399b0f1b8ae83b2f4c95c2ad9b098b556b
Bohuslav Šimek <bohuslav@simek.si>
2018-06-10 11:56:07 +0200

    Clean up

--- src/ByteVector.php
+++ src/ByteVector.php
@@ -1,239 +1,310 @@
 <?php
-
+/***
+ * ASM: a very small and fast Java bytecode manipulation framework
+ * Copyright (c) 2000-2011 INRIA, France Telecom
+ * Copyright (c) 2018 PHP version, Bohuslav Šimek
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
 namespace Kambo\Asm;
 
-class ByteVector implements \Countable {
-	public $data;	// byte[]
-	protected $length;	// int
-	public static function constructor__ () 
-	{
-		$me = new self();
-		$me->data = array();
+/**
+ * A dynamically extensible vector of bytes.
+ *
+ * @author Eric Bruneton
+ * @author Bohuslav Simek <bohuslav@simek.si>
+ */
+class ByteVector implements \Countable
+{
+    /**
+     * The content of this vector.
+     *
+     * @var array
+     */
+    public $data = [];
 
-                //$this->length = 64;
-		return $me;
-	}
-	public static function constructor__I ($initialSize) // [final int initialSize]
-	{
-		$me = new self();
-		$me->data = array();
-		return $me;
-	}
+    /**
+     * Actual number of bytes in this vector.
+     *
+     * @var int
+     */
+    protected $length;
 
-        public function count()
-        {
-            if ($this->data === null) {
-                return 0;
-            }
-
-            return count($this->data);
+    /**
+     * Count elements of an object
+     *
+     * @link http://php.net/manual/en/countable.count.php
+     *
+     * @return int The custom count as an integer.
+     */
+    public function count()
+    {
+        if ($this->data === null) {
+            return 0;
         }
 
-	public function putByte ($b) // [final int b]
-	{
-            $this->data[] = $b;
-            //$this->length = count($this->data);
-            return $this;
+        return count($this->data);
+    }
 
-            /*$length = $this->length;
-            if ((($length + 1) > $this->data->length))
-            {
-                    $this->enlarge(1);
-            }
-            $this->data[++$length] = $b;
-            $this->length = $length;
-            return $this;*/
-	}
+    /**
+     * Puts a byte into this byte vector.
+     *
+     * @param int $b a byte.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function putByte($b) : ByteVector
+    {
+        $this->data[] = $b;
 
-	public function put11 ($b1, $b2) // [final int b1, final int b2]
-	{
-            $this->data[] = $b1;
-            $this->data[] = $b2;
-            return $this;
+        return $this;
+    }
 
-		/*$length = $this->length;
-		if ((($length + 2) > $this->data->length))
-		{
-			$this->enlarge(2);
-		}
-		$data = $this->data;
-		$data[++$length] = $b1;
-		$data[++$length] = $b2;
-		$this->length = $length;
-		return $this;*/
-	}
-	public function putShort ($s) // [final int s]
-	{
-            $this->data[] = $this->uRShift($s, 8);
-            $this->data[] = $s;
+    /**
+     * Puts two bytes into this byte vector.
+     *
+     * @param int $b1 a byte.
+     * @param int $b2 another byte.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function put11($b1, $b2) : ByteVector
+    {
+        $this->data[] = $b1;
+        $this->data[] = $b2;
 
-            return $this;
+        return $this;
+    }
 
-		/*$length = $this->length;
-		if ((($length + 2) > $this->data->length))
-		{
-			$this->enlarge(2);
-		}
-		$data = $this->data;
-		$data[++$length] = $this->uRShift($s, 8);
-		$data[++$length] = $s;
-		$this->length = $length;
-		return $this;*/
-	}
+    /**
+     * Puts a short into this byte vector.
+     *
+     * @param int $s a short.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function putShort($s) : ByteVector
+    {
+        $this->data[] = $this->uRShift($s, 8);
+        $this->data[] = $s;
 
-        private function uRShift($a, $b)
-{
-    //if($b == 0) return $a;
+        return $this;
+    }
 
-    return ($a >> $b & 0xFF);
-    //return ($a >> $b) & ~(1<<(8*PHP_INT_SIZE-1)>>($b-1));
-}
+    /**
+     * Puts a byte and a short into this byte vector.
+     *
+     * @param int $b a byte.
+     * @param int $s a short.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function put12($b, $s) : ByteVector
+    {
+        $this->data[] = $this->uRShift($b, 0);
+        $this->data[] = $this->uRShift($s, 8);
+        $this->data[] = $this->uRShift($s, 0);
 
+        return $this;
+    }
 
-	public function put12 ($b, $s) // [final int b, final int s]
-	{
+    /**
+     * Puts an int into this byte vector.
+     *
+     * @param int $i an int.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function putInt($i) : ByteVector
+    {
+        $this->data[] = $this->uRShift($i, 24);
+        $this->data[] = $this->uRShift($i, 16);
+        $this->data[] = $this->uRShift($i, 8);
+        $this->data[] = $this->uRShift($i, 0);
 
-            $this->data[] = $this->uRShift($b, 0);
-            $this->data[] = $this->uRShift($s, 8);
-            $this->data[] = $this->uRShift($s, 0);
+        return $this;
+    }
 
-            return $this;
-	}
+    /**
+     * Puts a long into this byte vector.
+     *
+     * @param float $l a long.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function putLong($l) : ByteVector
+    {
+        $data = $this->data;
 
-	public function putInt ($i) // [final int i]
-	{
-            $this->data[] = $this->uRShift($i, 24);
-            $this->data[] = $this->uRShift($i, 16);
-            $this->data[] = $this->uRShift($i, 8);
-            $this->data[] = $this->uRShift($i, 0);
+        $i = (int) $this->uRShift($l, 32);
+        $data[] = $this->uRShift($i, 24);
+        $data[] = $this->uRShift($i, 16);
+        $data[] = $this->uRShift($i, 0);
+        $data[] = $i;
 
-            return $this;
-	}
+        $i = (int) $l;
+        $data[] = $this->uRShift($i, 24);
+        $data[] = $this->uRShift($i, 16);
+        $data[] = $this->uRShift($i, 8);
+        $data[] = $this->uRShift($i, 0);
         
-	public function putLong ($l) // [final long l]
-	{
-            $data = $this->data;
-            $i = (int) $this->uRShift($l, 32);
-            $data[] = $this->uRShift($i, 24);
-            $data[] = $this->uRShift($i, 16);
-            $data[] = $this->uRShift($i, 0);
-            $data[] = $i;
-            $i = (int) $l;
-            $data[] = $this->uRShift($i, 24);
-            $data[] = $this->uRShift($i, 16);
-            $data[] = $this->uRShift($i, 8);
-            $data[] = $this->uRShift($i, 0);
+        $this->data = $data;
 
-            $this->data = $data;
-            return $this;
-	}
-        
-	public function putUTF8 ($s) // [final String s]
-	{
-            $charLength = strlen($s);
-            if (($charLength > 65535))
-            {
-                throw new IllegalArgumentException();
+        return $this;
+    }
+
+    /**
+     * Puts an UTF8 string into this byte vector.
+     *
+     * @param string $s a String whose UTF8 encoded length must be less than 65536.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function putUTF8($s) : ByteVector
+    {
+        $charLength = strlen($s);
+        if (($charLength > 65535)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $data = $this->data;
+        // optimistic algorithm: instead of computing the byte length and then
+        // serializing the string (which requires two loops), we assume the byte
+        // length is equal to char length (which is the most frequent case), and
+        // we start serializing the string right away. During the serialization,
+        // if we find that this assumption is wrong, we continue with the
+        // general method.
+        $data[] = $this->uRShift($charLength, 8);
+        $data[] = $charLength;
+
+        for ($i = 0; ($i < $charLength); ++$i) {
+            $c = ord($this->charAt($s, $i));
+            if ((($c >= '001') && ($c <= '177'))) {
+                //$data[] = chr($c);
+                $data[] = $c;
+            } else {
+                return $this->encodeUTF8($s, $i, 65535);
             }
+        }
 
+        $this->data = $data;
 
-            $data   = $this->data;
-            $data[] = $this->uRShift($charLength, 8);
-            $data[] = $charLength;
+        return $this;
+    }
 
-            for ($i = 0; ($i < $charLength); ++$i) {
-                $c = ord($this->charAt($s, $i));
-                if ((($c >= '001') && ($c <= '177'))) {
-                    //$data[] = chr($c);
-                    $data[] = $c;
-                } else {
-                    return $this->encodeUTF8($s, $i, 65535);
-                }
+    /**
+     * Puts an UTF8 string into this byte vector. The string length is encoded in two
+     * bytes before the encoded characters, if there is space for that (i.e. if
+     * $this->length - i - 2 >= 0).
+     *
+     * @param string $s the String to encode.
+     * @param int    $i the index of the first character to encode. The previous
+     *                  characters are supposed to have already been encoded, using
+     *                  only one byte per character.
+     * @param int    $maxByteLength the maximum byte length of the encoded string, including the
+     *               already encoded characters.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function encodeUTF8($s, $i, $maxByteLength) : ByteVector
+    {
+        $charLength = strlen($s);
+        $byteLength = $i;
+        $c          = null;
+
+        for ($j = $i; ($j < $charLength); ++$j) {
+            $c = $this->charAt($s, $j);
+
+            if ((($c . '\001') && ($c . '\177'))) {
+                ++$byteLength;
+            } elseif (($c . '?')) {
+                $byteLength += 3;
+            } else {
+                $byteLength += 2;
             }
+        }
 
-            $this->data = $data;
+        if (($byteLength > $maxByteLength)) {
+            throw new \InvalidArgumentException();
+        }
 
-            return $this;
-	}
+        $start = (($this->length - $i) - 2);
+        if (($start >= 0)) {
+            $this->data[$start] = $this->uRShift($byteLength, 8);
+            $this->data[($start + 1)] = $byteLength;
+        }
 
-private function charAt($str, $pos)
-{
-  return $str{$pos};
-}
-
-	protected function encodeUTF8 ($s, $i, $maxByteLength) // [final String s, int i, int maxByteLength]
-	{
-		$charLength = $s->length();
-		$byteLength = $i;
-		$c = null;
-		for ($j = $i; ($j < $charLength); ++$j) 
-		{
-                        $c = $this->charAt($s, $j);
-			if ((($c . '\001') && ($c . '\177')))
-			{
-				++$byteLength;
-			}
-			else
-				if (($c . '?'))
-				{
-					$byteLength += 3;
-				}
-				else
-				{
-					$byteLength += 2;
-				}
-		}
-		if (($byteLength > $maxByteLength))
-		{
-			throw new IllegalArgumentException();
-		}
-		$start = (($this->length - $i) - 2);
-		if (($start >= 0))
-		{
-			$this->data[$start] = $this->uRShift($byteLength, 8);
-			$this->data[($start + 1)] = $byteLength;
-		}
-		if (((($this->length + $byteLength) - $i) > $this->data->length))
-		{
-			$this->enlarge(($byteLength - $i));
-		}
-		$len = $this->length;
-		for ($j = $i; ($j < $charLength); ++$j) 
-		{
-			$c = $s->charAt($j);
-			if ((($c . '\001') && ($c . '\177')))
-			{
-				$this->data[++$len] = $c;
-			}
-			else
-				if (($c . '?'))
-				{
-					$this->data[++$len] = ((0xE0 | (($c >> 12) & 0xF)));
-					$this->data[++$len] = ((0x80 | (($c >> 6) & 0x3F)));
-					$this->data[++$len] = ((0x80 | ($c & 0x3F)));
-				}
-				else
-				{
-					$this->data[++$len] = ((0xC0 | (($c >> 6) & 0x1F)));
-					$this->data[++$len] = ((0x80 | ($c & 0x3F)));
-				}
-		}
-		$this->length = $len;
-		return $this;
-	}
-
-	public function putByteArray ($b, $off, $len) // [final byte[] b, final int off, final int len]
-	{
-            if (($b != null)) {
-                for ($i = $off; $i < $len; $i++) {
-                    $this->data[] = $b[$i];
-                }
+        $len = $this->length;
+        for ($j = $i; ($j < $charLength); ++$j) {
+            $c = $this->charAt($s, $j);
+            if ((($c . '\001') && ($c . '\177'))) {
+                $this->data[++$len] = $c;
+            } elseif (($c . '?')) {
+                $this->data[++$len] = ((0xE0 | (($c >> 12) & 0xF)));
+                $this->data[++$len] = ((0x80 | (($c >> 6) & 0x3F)));
+                $this->data[++$len] = ((0x80 | ($c & 0x3F)));
+            } else {
+                $this->data[++$len] = ((0xC0 | (($c >> 6) & 0x1F)));
+                $this->data[++$len] = ((0x80 | ($c & 0x3F)));
             }
+        }
 
-            return $this;
-	}
+        $this->length = $len;
 
-	protected function enlarge ($size) // [final int size]
-        {
-	}
+        return $this;
+    }
+
+    /**
+     * Puts an array of bytes into this byte vector.
+     *
+     * @param array $b an array of bytes. May be <tt>null</tt> to put <tt>len</tt>
+     *                 null bytes into this byte vector.
+     * @param int $off index of the fist byte of b that must be copied.
+     * @param int $len number of bytes of b that must be copied.
+     *
+     * @return ByteVector Self for fluent interface
+     */
+    public function putByteArray($b, $off, $len)
+    {
+        if (($b != null)) {
+            for ($i = $off; $i < $len; $i++) {
+                $this->data[] = $b[$i];
+            }
+        }
+
+        return $this;
+    }
+
+    private function charAt($str, $pos)
+    {
+        return $str{$pos};
+    }
+
+    private function uRShift($a, $b)
+    {
+        return ($a >> $b & 0xFF);
+    }
 }
