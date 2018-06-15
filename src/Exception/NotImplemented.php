91a246aa4ff17539a33904bed5c48de366514fa8
Bohuslav Šimek <bohuslav@simek.si>
2018-06-15 20:14:36 +0200

    Add not implemeted exception

---
+++ src/Exception/NotImplemented.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kambo\Karsk\Exception;
+
+/**
+ * Not implemented exception
+ *
+ * @author  Bohuslav Simek <bohuslav@simek.si>
+ * @license MIT
+ */
+class NotImplemented extends \Exception
+{
+
+}
