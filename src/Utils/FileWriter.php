f620f81128d584f82d51f51d9aa38cec6b95742e
Bohuslav Šimek <bohuslav@simek.si>
2018-07-14 16:43:03 +0200

    Add method for saving generated class into the file

---
+++ src/Utils/FileWriter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kambo\Karsk\Utils;
+
+use Kambo\Karsk\ClassWriter;
+
+/**
+ * Save generated class into the file
+ *
+ * @author  Bohuslav Simek <bohuslav@simek.si>
+ * @license BSD-3-Clause
+ */
+class FileWriter
+{
+    /**
+     * Save generated class into the file
+     *
+     * @param ClassWriter $classWriter Class definition
+     * @param string      $fileName    Class filename
+     *
+     * @return void
+     */
+    public function writeClassFile(ClassWriter $classWriter, string $fileName) : void
+    {
+        $code = $classWriter->toByteArray();
+
+        $binaryString = pack("c*", ...$code);
+
+        $file = fopen($fileName, 'w+');
+
+        fwrite($file, $binaryString);
+        fclose($file);
+    }
+}
